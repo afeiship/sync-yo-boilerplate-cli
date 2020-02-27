d289c2fa0ed3594b1ba58af0b557b0053b7d4198
afeiship <1290657123@qq.com>
2020-02-27 21:18:34 +0800

    feat: add tty spinner

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # sync-yo-boilerplate
 > Sync yeoman boilerplate cache.
+
+## debug
+```shell
+ruby ./src/index.rb sync boilerplate-webkit-sassui
+```
+
+## installation
+```shell
+npm install -S @feizheng/sync-yo-boilerplate
+```
+
+## usage
+```shell
+thor thor_cli:sync_yo_boilerplate:sync boilerplate-webkit-sassui
+```
--- src/index.rb
+++ src/index.rb
@@ -2,6 +2,7 @@
 
 require "thor"
 require "fileutils"
+require "tty-spinner"
 
 YO_CACHE = "/Users/feizheng/.cache/node-yeoman-remote-cache/afeiship"
 
@@ -10,15 +11,25 @@ module ThorCli
     desc "sync NAME", "Sync github boilerplate to yeoman cache."
 
     def sync(name)
+      spinners = TTY::Spinner::Multi.new("[:spinner] task start", format: :spin)
+      sp1 = spinners.register "[:spinner] 1. clean old files"
+      sp2 = spinners.register "[:spinner] 2. get files with proxy from github: #{name}"
+
       dir = "#{YO_CACHE}/#{name}"
       FileUtils.cd(YO_CACHE, :verbose => true) do
+
+        sp1.auto_spin
         # clean old
         system "rm -rf #{name}"
         system "mkdir -p #{name}/master"
 
+        sp1.success
+
+        sp2.auto_spin
         # download lastet
-        system "https_proxy=http://127.0.0.1:9090 wget --directory-prefix=#{dir} https://github.com/afeiship/#{name}/archive/master.tar.gz"
+        system "https_proxy=http://127.0.0.1:9090 wget -q  --directory-prefix=#{dir} https://github.com/afeiship/#{name}/archive/master.tar.gz"
         system "gtar -zxf #{name}/master.tar.gz --strip-components 1  --directory #{name}/master"
+        sp2.success
       end
     end
 
